dfe2938eaae9b8e8dbb8ff2ba0eed5429c53e88b
Patrick Zeinert <patrick@bryanbroadcasting.com>
2013-07-29 17:22:55 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<head>
+    <!-- ! Vlntr.Me - v0.1 - 2013-07-29 -->
+    <title>Vlntr.Me | Volunteer Your Mind, Body and Soul</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+    <link href='style/main.css' rel='stylesheet' type='text/css'>
+    <link href='style/font-awesome.css' rel='stylesheet' type='text/css'>
+    <meta property="og:image" content="img/logo.jpg"/>
+</head>
+<body>
+<div class="center">
+    <article class="introduction">
+        <header>
+            <h1 class="page-title">Vlntr.Me</h1>
+            <h2>Volunteer Your Mind, Body and Soul</h2>
+        </header>
+        <div class="content">
+        	<h1>Day 1 (July 29, 2013)</h1>
+            <p><span class="firstcharacter">V</span>olunteerism is one of the best things we have going for us. The desire to serve is rooted in our psyche, but can be overwhelmed by other commitments and temptations. What Vlntr.Me seeks to do is <strong>facilitate the discovery of volunteer opportunities for individuals and groups in their respective communities</strong>.</p>
+
+            <p>There is already one <a href="http://volunteermatch.org" target="_blank" alt="Volunteer Match">website that does this well</a>, but we feel that it is targeted towards the organizations and not individuals. We aren't seeking to compete with that model because we believe Vlntr.Me will help serve in a slightly different way; namely by focusing on the <em>communities</em> that the organizations reside in and not the organization itself.</p>
+
+            <p>We don't have anything to offer at this point except a promise — maybe “goal” is a better way to say it. Our goal is to <strong>help people find volunteer opportunities when and where they are free to volunteer</strong>. We want to encourage individuals and groups to participate regularly and across all portions of their community, not just on their block or in their congregation. As updates are available we will detail them here.</p>
+
+            <p>Until we are able to better explain how we want to reach our goals, you can <a href="mailto:contact@vlntr.me">contact us</a> for more specific information and to see how you can help. But don't wait for us to get up and running before you find a reason to go volunteer. Do it now!</p>
+
+        <blockquote><em>&#8220;You never change things by fighting the existing reality. To change something, build a new model that makes the existing model obsolete.&#8221;</em> &mdash; Buckminster Fuller</blockquote>
+    </article>
+</div>
+<aside class="link-list">
+    <h3>Resources</h3>
+    <ul>
+        <li class="icon-github"><a href="https://github.com/CoeusCC/vlntr.me">GitHub Repo</a></li>
+        <li class="icon-facebook"><a href="http://www.facebook.com/vlntr.me">Facebook</a></li>
+        <li class="icon-twitter"><a href="https://twitter.com/Vlntr_Me">Twitter</a></li>
+        <li><a href="http://iginomarini.com/fell/the-revival-fonts/">The Fell Types</a></li>
+    </ul>
+</aside>
+<footer>
+
+<span>&copy; Copyright 2013</span>
+</footer>
+</body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40392181-3', 'vlntr.me');
+  ga('send', 'pageview');
+
+</script>
+</html>
